7fb2de5f88edde61e89de953c0150612c6cb5b0d
ohsj <dhtmdwns319@naver.com>
2025-10-30 14:02:25 +0900

    base 수정

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/DevOps-IMP-QAD/',
   plugins: [react()],
   server: {
     proxy: {
